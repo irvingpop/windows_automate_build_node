5b38abbde573d032598776d83ed353f299ab55b9
Irving Popovetsky <irving@chef.io>
2016-12-22 16:02:23 -0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# windows_automate_build_node
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: windows_automate_build_node
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
